2ec69fd78487e0619467be960ea98e06e947490b
Ga-byte-l <124946471+Ga-byte-l@users.noreply.github.com>
2023-03-15 18:35:16 -0300

    Add files via upload
    initial commit

---
+++ comportamiento.js
@@ -0,0 +1,15 @@
+let body=document.body;
+
+
+function darkmode(){
+    
+    /*document.body.classList.toggle('dark_mode');*/
+   body.classList.toggle('dark_mode');
+}
+function mostrar(){
+    document.getElementById('descripcion').style.display= 'block';
+}
+function ocultar(){}
+    function ocultar(){
+        document.getElementById('descripcion').style.display= 'none';
+    }
